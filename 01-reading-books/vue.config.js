8856095ad72a6ec431448b32ee118297e248f9b3
Jesús Lorenzo Limón <jlorenzolimon@gmail.com>
2024-07-07 12:06:32 +0200

    apartado de producción

--- 01-reading-books/vue.config.js
+++ 01-reading-books/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/pruebas-tecnicas/",
+  publicPath: "/pruebas-tecnicas/01-reading-books/",
 })
